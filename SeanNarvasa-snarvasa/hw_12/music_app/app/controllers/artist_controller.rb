86f01a8c3e428fb3fa29cce9c1fd39c5b88a2a7c
Sean Narvasa <sean@snarvasa.com>
2015-09-16 18:04:16 -0700

    initial commit

---
+++ SeanNarvasa-snarvasa/hw_12/music_app/app/controllers/artist_controller.rb
@@ -0,0 +1,56 @@
+class ArtistController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = get_artist
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @artist = get_artist
+  end
+
+  def update
+    @artist = get_artist
+
+    if @artist.update(artist_params)
+      redirect_to @artist
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @artist = get_artist
+    @artist.destroy
+
+    redirect_to artist_path
+  end
+
+  private
+
+    def artist_params
+      params.require(:artist).permit(:name, :description, :image_url)
+    end
+
+    def get_artist
+      @artist = Artist.find(params[:id])
+    end
+
+end
